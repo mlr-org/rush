9f020ae865b60a99c1eed291c04dbf913c95306c
be-marc <marcbecker@posteo.de>
2023-10-13 17:32:40 +0200

    test: remove helper

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -17,13 +17,6 @@ expect_reset_rush = function(rush) {
   expect_list(rush$connector$command(c("KEYS", "*")), len = 0)
 }
 
-expect_stopped_workers = function(pids) {
-  pid_exists = choose_pid_exists()
-  walk(pids, function(pid) {
-    expect_false(pid_exists(pid))
-  })
-}
-
 clean_test_env = function(pids) {
   walk(pids, tools::pskill)
   future::plan("sequential")
