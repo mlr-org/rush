ecedc046792e5f482d246cdf849d601773beeb91
be-marc <marcbecker@posteo.de>
2023-10-23 02:55:47 -0600

    benchmark: 2023-10-22 beartooth

--- benchmark/batchtools.R
+++ benchmark/batchtools.R
@@ -1,13 +1,14 @@
 library(batchtools)
 
-# reg = makeRegistry(file.dir = "/gscratch/mbecke16/2023-10-13-rush", conf.file = "benchmark/batchtools.conf.R", packages = "renv", seed = 1)
-reg = loadRegistry(file.dir = "/gscratch/mbecke16/2023-10-13-rush", writeable = TRUE)
+# reg = makeRegistry(file.dir = "/gscratch/mbecke16/benchmark-rush", conf.file = "benchmark/batchtools.conf.R", packages = "renv", seed = 1)
+# reg = loadRegistry(file.dir = "/gscratch/mbecke16/benchmark-rush", writeable = TRUE)
+# unlink("/gscratch/mbecke16/benchmark-rush", recursive = TRUE)
 
 source("benchmark/runner.R")
 
 batchMap(
   fun = runner,
-  renv_project = list("benchmark/snapshot-2023-10-13"),
+  renv_project = list("benchmark/snapshot-2023-10-22"),
   times = list(100)
 )
 
--- benchmark/setup.R
+++ benchmark/setup.R
@@ -1,9 +1,20 @@
 dir.create("benchmark/snapshot-2023-10-13")
 
-# before workshop
 renv::init("benchmark/snapshot-2023-10-13", bare = TRUE)
 renv::load("benchmark/snapshot-2023-10-13")
 renv::settings$snapshot.type("all")
 renv::install(c("microbenchmark", "mlr-org/rush@8cd1635d911f731ee8052c7e29ba4d92cfe7bd78"))
 renv::snapshot()
 # renv::restore()
+
+dir.create("benchmark/snapshot-2023-10-22")
+renv::init("benchmark/snapshot-2023-10-22", bare = TRUE)
+renv::load("benchmark/snapshot-2023-10-22")
+renv::settings$snapshot.type("all")
+renv::install(c("microbenchmark", "mlr-org/rush@99ff9476d03a3d32a5679a53c8303007de707498"))
+renv::snapshot()
+# renv::restore()
+
+
+
+
