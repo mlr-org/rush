4708a404c5f69b5352eab6262e2621eea4916ec7
be-marc <marcbecker@posteo.de>
2024-04-15 10:22:33 +0200

    feat: add rush assertions

---
+++ R/assertions.R
@@ -0,0 +1,52 @@
+#' @title Assertion for Rush Objects
+#'
+#' @description
+#' Most assertion functions ensure the right class attribute, and optionally additional properties.
+#' If an assertion fails, an exception is raised.
+#' Otherwise, the input object is returned invisibly.
+#'
+#' @name rush_assertions
+#' @keywords internal
+NULL
+
+#' @export
+#' @param rush ([Rush]).
+#' @param null_ok (`logical(1)`).
+#'  If `TRUE`, `NULL` is allowed.
+#' @rdname rush_assertions
+assert_rush = function(rush, null_ok = FALSE) {
+  if (null_ok && is.null(rush)) invisible(return(NULL))
+  assert_r6(rush, "Rush")
+  invisible(rush)
+}
+
+#' @export
+#' @param rushs (list of [Rush]).
+#' @param null_ok (`logical(1)`).
+#'  If `TRUE`, `NULL` is allowed.
+#' @rdname rush_assertions
+assert_rushs = function(rushs, null_ok = FALSE) {
+  if (null_ok && is.null(rushs)) invisible(return(NULL))
+  invisible(map(rushs, assert_rush))
+}
+
+#' @export
+#' @param worker ([RushWorker]).
+#' @param null_ok (`logical(1)`).
+#'  If `TRUE`, `NULL` is allowed.
+#' @rdname rush_assertions
+assert_rush_worker = function(worker, null_ok = FALSE) {
+  if (null_ok && is.null(worker)) invisible(return(NULL))
+  assert_r6(worker, "RushWorker")
+  invisible(worker)
+}
+
+#' @export
+#' @param workers (list of [RushWorker]).
+#' @param null_ok (`logical(1)`).
+#'  If `TRUE`, `NULL` is allowed.
+#' @rdname rush_assertions
+assert_rush_workers = function(workers, null_ok = FALSE) {
+  if (null_ok && is.null(workers)) invisible(return(NULL))
+  invisible(map(workers, assert_rush_worker))
+}
