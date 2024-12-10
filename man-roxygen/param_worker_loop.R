c0640777af749425d1f28b8a4f859266b274928b
be-marc <marcbecker@posteo.de>
2024-12-10 18:15:41 +0100

    ...

--- man-roxygen/param_worker_loop.R
+++ man-roxygen/param_worker_loop.R
@@ -1,5 +1,2 @@
 #' @param worker_loop (`function`)\cr
 #' Loop run on the workers.
-#' Defaults to [worker_loop_default] which is called with `fun`.
-#' Pass `fun` in `...`.
-#' Use [worker_loop_callr] to run `fun` in an external callr session.
