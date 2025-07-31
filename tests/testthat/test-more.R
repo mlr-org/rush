8aee3d9ad6852810b065485a6d2fe5d489eb2653
be-marc <marcbecker@posteo.de>
2025-07-31 11:43:31 +0200

    ...

--- tests/testthat/test-more.R
+++ tests/testthat/test-more.R
@@ -136,3 +136,71 @@ test_that("segfaults on workers are detected via the heartbeat", {
 
   expect_rush_reset(rush)
 })
+
+test_that("a simple error is catched", {
+  skip_on_cran()
+
+  config = start_flush_redis()
+  rush = rsh(network_id = "test-rush", config = config)
+
+  worker_loop = function(rush) {
+    while(!rush$terminated && !rush$terminated_on_idle) {
+      task = rush$pop_task(fields = c("xs", "seed"))
+      if (!is.null(task)) {
+        tryCatch({
+          fun = function(x1, x2, ...) {
+            if (x1 < 1) stop("Test error")
+            list(y = x1 + x2)
+          }
+          ys = with_rng_state(fun, args = c(task$xs), seed = task$seed)
+          rush$push_results(task$key, yss = list(ys))
+        }, error = function(e) {
+          condition = list(message = e$message)
+          rush$push_failed(task$key, conditions = list(condition))
+        })
+      }
+    }
+
+    return(NULL)
+  }
+
+  worker_ids = rush$start_local_workers(
+    worker_loop = worker_loop,
+    n_workers = 4,
+    lgr_thresholds = c("mlr3/rush" = "debug"))
+  rush$wait_for_workers(2, timeout = 5)
+
+  xss = list(list(x1 = 1, x2 = 2), list(x1 = 0, x2 = 2))
+  keys = rush$push_tasks(xss)
+  rush$wait_for_tasks(keys, detect_lost_workers = TRUE)
+  Sys.sleep(2)
+
+  # check task count
+  expect_equal(rush$n_tasks, 2)
+  expect_equal(rush$n_queued_tasks, 0)
+  expect_equal(rush$n_running_tasks, 0)
+  expect_equal(rush$n_finished_tasks, 1)
+  expect_equal(rush$n_failed_tasks, 1)
+
+  # check keys in sets
+  expect_character(rush$tasks, len = 2)
+  expect_null(rush$queued_tasks)
+  expect_null(rush$running_tasks)
+  expect_string(rush$finished_tasks)
+  expect_string(rush$failed_tasks)
+
+  # check fetching
+  expect_data_table(rush$fetch_queued_tasks(), nrows = 0)
+  expect_data_table(rush$fetch_running_tasks(), nrows = 0)
+  expect_data_table(rush$fetch_tasks(), nrows = 2)
+
+  data = rush$fetch_finished_tasks()
+  expect_names(names(data), must.include = c("x1", "x2", "worker_id", "y", "keys"))
+  expect_data_table(data, nrows = 1)
+
+  data = rush$fetch_failed_tasks()
+  expect_names(names(data), must.include = c("x1", "x2", "worker_id", "message", "keys"))
+  expect_data_table(data, nrows = 1)
+
+  expect_rush_reset(rush)
+})
