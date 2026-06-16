0d548bb73560393a7572e9c6e3d71384ca784bf7
be-marc <marcbecker@posteo.de>
2026-06-16 18:22:20 +0200

    docs: store_large_object function

--- R/store_large_object.R
+++ R/store_large_object.R
@@ -2,11 +2,14 @@
 #'
 #' @description
 #' Store large objects to disk and return a reference to the object.
+#' The written `.rds` files are not cleaned up automatically.
+#' The caller is responsible for removing them, e.g. by storing them below [tempdir()] or by calling [unlink()] on the
+#' returned `path`.
 #'
 #' @param obj (`any`)\cr
 #' Object to store.
 #' @param path (`character(1)`)\cr
-#' Path to store the object.
+#' Path to an existing directory to store the object in.
 #'
 #' @return `list()` of class `"rush_large_object"` with the name and path of the stored object.
 #' @export
@@ -14,7 +17,7 @@
 #' obj = list(a = 1, b = 2)
 #' rush_large_object = store_large_object(obj, tempdir())
 store_large_object = function(obj, path) {
-  assert_string(path)
+  assert_directory_exists(path)
 
   name = deparse(substitute(obj))
   id = uuid::UUIDgenerate()
