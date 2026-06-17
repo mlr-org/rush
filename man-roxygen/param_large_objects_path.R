5d3cadd28e6daaf9611ff8550e179381a740cf61
be-marc <marcbecker@posteo.de>
2026-06-17 16:51:04 +0200

    docs: enhance documentation for large_objects_path parameter

--- man-roxygen/param_large_objects_path.R
+++ man-roxygen/param_large_objects_path.R
@@ -1,2 +1,5 @@
 #' @param large_objects_path (`character(1)`)\cr
 #' The path to the directory where large objects are stored.
+#' These files are not removed automatically
+#' and the caller is responsible for cleaning them up,
+#' e.g. by pointing this at a subdirectory of [tempdir()].
